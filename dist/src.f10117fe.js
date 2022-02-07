29972bb957c1fac9907c07922b67af4c4f545aed
Micheala Palmer <michealapalmer@gmail.com>
2022-02-07 00:01:16 -0500

    create Company class and new instance of that class

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -136868,7 +136868,39 @@ exports['zh_TW'] = require('./locales/zh_TW');
 var Faker = require('./lib');
 var faker = new Faker({ locales: require('./lib/locales') });
 module['exports'] = faker;
-},{"./lib":"node_modules/faker/lib/index.js","./lib/locales":"node_modules/faker/lib/locales.js"}],"src/User.ts":[function(require,module,exports) {
+},{"./lib":"node_modules/faker/lib/index.js","./lib/locales":"node_modules/faker/lib/locales.js"}],"src/Company.ts":[function(require,module,exports) {
+"use strict";
+
+var __importDefault = this && this.__importDefault || function (mod) {
+  return mod && mod.__esModule ? mod : {
+    "default": mod
+  };
+};
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Company = void 0;
+
+var faker_1 = __importDefault(require("faker"));
+
+var Company =
+/** @class */
+function () {
+  function Company() {
+    this.companyName = faker_1.default.company.companyName();
+    this.catchPhrase = faker_1.default.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker_1.default.address.latitude()),
+      lng: parseFloat(faker_1.default.address.longitude())
+    };
+  }
+
+  return Company;
+}();
+
+exports.Company = Company;
+},{"faker":"node_modules/faker/index.js"}],"src/User.ts":[function(require,module,exports) {
 "use strict";
 
 var __importDefault = this && this.__importDefault || function (mod) {
@@ -136906,11 +136938,15 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var Company_1 = require("./Company");
+
 var User_1 = require("./User");
 
 var user = new User_1.User();
 console.log(user);
-},{"./User":"src/User.ts"}],"../../.nvm/versions/node/v12.22.7/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var company = new Company_1.Company();
+console.log(company);
+},{"./Company":"src/Company.ts","./User":"src/User.ts"}],"../../.nvm/versions/node/v12.22.7/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -136938,7 +136974,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35259" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46183" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+import { Company } from './Company';
 import { User } from './User';
 
 const user = new User();
 console.log(user);
+
+const company = new Company();
+console.log(company);
