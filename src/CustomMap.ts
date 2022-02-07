26cb080c7c8badf178deeccfbca66478315e56af
Micheala Palmer <michealapalmer@gmail.com>
2022-02-07 15:56:06 -0500

    create map class and new instance, add markers/event listener

---
+++ src/CustomMap.ts
@@ -0,0 +1,37 @@
+//show a google map and markers
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0
+      }
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow =  new google.maps.InfoWindow({
+        content: 'Hi there!'
+      });
+      infoWindow.open(this.googleMap, marker)
+    });
+  }
+}
